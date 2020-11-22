4c0ea4873fb2871a57e500383b03dd9aad9c9f39
StamirskiK <01123929@pw.edu.pl>
2020-11-22 20:53:01 +0000

    rev01

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,17 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int ltowarow = 0;
+    unsigned int lzaglowcow = 0;
+
+    while (towar > ltowarow){
+      Stocznia stocznia1{};
+      Statek* statek1 = stocznia1();
+      ltowarow += statek1->transportuj();
+      Zaglowiec* wsk_zagl = dynamic_cast< Zaglowiec* >(statek1);
+      if (wsk_zagl != 0)
+      lzaglowcow ++;
+      delete statek1;
+    }
+    return lzaglowcow;
 }
